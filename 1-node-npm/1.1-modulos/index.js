7ebcf458cb635f3739a3ce6526437789e9d33112
Edualf Pacheco Quintero <edualf.quintero@coppel.com>
2022-10-19 18:09:54 -0600

    Commit 2

---
+++ 1-node-npm/1.1-modulos/index.js
@@ -0,0 +1,11 @@
+import { sum, substract } from '../1.1-modulos/sum-substract';
+import { multiply, divide } from '../1.1-modulos/multiple-divide';
+
+function main(){
+    const num1 = 20;
+    const num2 = 5;
+    const sumResult = sum(num1, num2);
+    console.log(sumResult);
+}
+
+main();
